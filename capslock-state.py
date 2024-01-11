b608da251b07dc2ed33265cd3c7813e191da3173
Straumleaf <straum.phone@gmail.com>
2024-01-11 20:38:41 +0200

    added function main and made some arrangements accordingly, also added some new shortcuts to sound files

--- capslock-state.py
+++ capslock-state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import os
+import os                                      # to run sound player and work with arguments
 import sys                                     # to work with arguments
-import pickle                                  # to work with serealizing/de-serealizing
+import pickle                                  # to work with serealizing/de-serealizing variable
 #import evdev                                  # to work with input devices
 from evdev import InputDevice, ecodes
 
@@ -10,21 +10,17 @@ from evdev import InputDevice, ecodes
 TMP_PATH = '/tmp/'
 CAPS_STATE_FILE = '_caps_led.state'
 
-# defining sound to play and sound player
+# defining sound player and sets of sounds to choose from
 SND_PLAYER = 'paplay'
 SND_FILE_BELL = '/usr/share/sounds/freedesktop/stereo/bell.oga'
+SND_FILE_MSG = '/usr/share/sounds/freedesktop/stereo/message.oga'
+SND_FILE_DLG_INF = '/usr/share/sounds/freedesktop/stereo/dialog-information.oga'
+SND_FILE_DLG_ERR = '/usr/share/sounds/freedesktop/stereo/dialog-error.oga'
 
 # list of app standard messages
 ERROR = 'Error!'
 CAPS_ON = 'Aa '
 CAPS_OFF = 'Aa '
-
-# Sound switch (by default - Off)  
-sound_sw = 'sound-off'
-
-# initializing Caps Lock state variables
-previous_caps_state = 0
-current_caps_state = 0
 
 # function to work with file were ops variable should have
 # such values as 'rb' - read, 'wb' - write and 'xb' - create only
@@ -40,47 +36,59 @@ def file_ops(ops, caps_state = None):
     except FileNotFoundError:
         file = open(TMP_PATH + CAPS_STATE_FILE, 'xb')
         caps_state = 0
-   
+
     file.close()
     return caps_state
 
 # function to play sound if Caps Lock state were changed
-def making_beep():
-   if previous_caps_state != current_caps_state:
+def making_beep(prv_state, curr_state):
+   if prv_state != curr_state:
       os.system(SND_PLAYER + ' ' + SND_FILE_BELL)
    
 # MAIN -------------------------------------------
 
-# reading args 
-# argv1 - device event path; argv2 - sound On/Off (default: Off)
-args_numb = len(sys.argv)
-match args_numb:
-    case 1:
-        state_msg = ERROR
-    case 2:
-        device_event_number = sys.argv[1]
-    case 3:
-        device_event_number = sys.argv[1]
-        sound_sw = sys.argv[2]
+def main(args):
+    # reading args 
+    # argv1 - device event path; argv2 - sound On/Off (default: Off)
+    args_numb = len(sys.argv)
 
-# keyboard descriptor
-keyboard = InputDevice(device_event_number)
+    # Sound switch (by default - Off)  
+    sound_sw = 'sound-off'
 
-# loading previous Caps Lock state if available
-previous_caps_state = file_ops('rb')
+    # initializing Caps Lock state variables
+    #previous_caps_state = 0
+    #current_caps_state = 0
 
-# reading current Caps Lock state from keyboard
-current_caps_state = keyboard.leds()
+    match args_numb:
+        case 1:
+            state_msg = ERROR
+        case 2:
+            device_event_number = args[1]
+        case 3:
+            device_event_number = args[1]
+            sound_sw = args[2]
 
-# determine and printing Caps Lock state
-if current_caps_state == [1]:
-    state_msg = CAPS_ON
-else:
-    state_msg = CAPS_OFF
+    # keyboard descriptor
+    keyboard = InputDevice(device_event_number)
 
-print (state_msg)
-# in case of changing states making a sound
-if sound_sw == 'sound-on':
-    making_beep()   
-# storing Caps Lock state in file and exit
-file_ops('wb', current_caps_state)
+    # loading previous Caps Lock state if available
+    previous_caps_state = file_ops('rb')
+
+    # reading current Caps Lock state from keyboard
+    current_caps_state = keyboard.leds()
+
+    # determine and printing Caps Lock state
+    if current_caps_state == [1]:
+        state_msg = CAPS_ON
+    else:
+        state_msg = CAPS_OFF
+
+    print(state_msg)
+    # in case of changing states making a sound
+    if sound_sw == 'sound-on':
+        making_beep(previous_caps_state, current_caps_state)   
+    # storing Caps Lock state in file and exit
+    file_ops('wb', current_caps_state)
+
+if __name__ == '__main__':
+    main(sys.argv)
